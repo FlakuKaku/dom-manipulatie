2278ba5ce9f8608495b50a22c2887e3c57275218
FlakuKaku <73279946+FlakuKaku@users.noreply.github.com>
2020-11-23 12:15:35 +0100

    Klaar

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
 const myName = 'Hi Paul';
 const messageElement = document.getElementById('message');
+const colors = ['red', 'green', 'blue', 'orange', 'yellow'];
+
 function myFunction() {
     if (messageElement.innerHTML === myName) {
         messageElement.innerHTML = '';
     } else {
         messageElement.innerHTML = myName;
     }
-}
+    let randomColor = colors[Math.floor(Math.random()*colors.length)];
+    messageElement.style.color = randomColor;
+};
 
