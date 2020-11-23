dfff32b684ecfcf87e405f56ece3e7d4c10555bf
FlakuKaku <73279946+FlakuKaku@users.noreply.github.com>
2020-11-23 11:17:13 +0100

    click interaction

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 const myName = 'Paul';
 const messageElement = document.getElementById('message').innerHTML += myName;
+function myFunction() {
+    var x = document.getElementById('message');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none'
+    }
+}
+
